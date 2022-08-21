4248aa92b3c97ddf0cc138a175b5685b8b60d8d9
Chen Xiao <ListenToATree@gmail.com>
2022-08-21 16:19:37 +0800

    add lib and demo

---
+++ projects/magic-strings/src/lib/magic-strings.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mg-magic-strings',
+  template: ` <p>magic-strings works! Test</p> `,
+  styles: [],
+})
+export class MagicStringsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
